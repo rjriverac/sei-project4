1194309974373ebe4b1d2e4abe97978fd4f8f6f3
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-02 18:44:41 +0100

    initial boilerplate for backend, started auth

---
+++ back-end/jwt_auth/views.py
@@ -0,0 +1,38 @@
+from django.http import response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
+from django.contrib.auth import get_user_model
+from django.conf import settings
+from .serializers import UserSerializer
+import jwt
+
+User = get_user_model()
+
+class RegisterView(APIView):
+    def post(self,request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'Registration successful'})
+        
+        return Response(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class LoginView(APIView):
+    def get_user(self, username):
+        try:
+            return User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise PermissionDenied({'message': 'Invalid credentials'})
+
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = self.get_user(username)
+        if not user.check_password(password):
+            raise PermissionDenied({'message': 'Invalid credentials'})
+
+        token = jwt.encode({'sub':user.id},settings.SECRET_KEY,algorithm='HS256')
+        return response({ 'token': token, 'message': f'Welcome back {user.username}'})
