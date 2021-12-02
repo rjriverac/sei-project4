399cd663d20d9fffb338cdd3ec7df1c286718dd6
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-02 23:57:31 +0100

    changed cart items structure

--- back-end/cart/models.py
+++ back-end/cart/models.py
@@ -5,9 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    item = models.ForeignKey("products.Product", on_delete= models.CASCADE)
-    quantity = models.IntegerField(default = 1)
+    order_items = models.ManyToManyField("products.Product")
 
     def __str__(self):
-        plural = 's' if self.quantity > 1 else ''
-        return f'{self.user.username} has {self.quantity} {self.item}{plural}' 
+        return f'{self.user.username} has {self.order_items} in their cart' 
