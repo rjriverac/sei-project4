41cc979efbe1e97b944d706bf24f297ddb859ca0
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-02 23:44:57 +0100

    started on cart model

--- back-end/cart/models.py
+++ back-end/cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    item = models.ForeignKey("products.Product", on_delete= models.CASCADE)
+    quantity = models.IntegerField(default = 1)
+
+    def __str__(self):
+        plural = 's' if self.quantity > 1 else ''
+        return f'{self.user.username} has {self.quantity} {self.item}{plural}' 
--- back-end/products/views.py
+++ back-end/products/views.py
@@ -1,9 +1,12 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser,IsAuthenticatedOrReadOnly
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
 
 # Create your views here.
 
@@ -15,6 +18,7 @@ class ProductListView(APIView):
         return Response(serialized_prods.data, status = status.HTTP_200_OK)
     
     def post(self, request):
+        self.permission_classes = IsAdminUser
         product = ProductSerializer(data = request.data)
         if product.is_valid():
             product.save()
@@ -22,3 +26,34 @@ class ProductListView(APIView):
         else:
             return Response(product.errors, status = status.HTTP_422_UNPROCESSABLE_ENTITY)
 
+class ProductDetailView(APIView):
+    def get(self,request,pk):
+        self.permission_classes = (IsAuthenticatedOrReadOnly)
+        try:
+            product = Product.objects.get(id=pk)
+            serialized_prod = ProductSerializer(product)
+            return Response(serialized_prod, status = status.HTTP_202_ACCEPTED)
+        except:
+            return Response(status.HTTP_404_NOT_FOUND)
+
+    def delete(self,request,pk):
+        self.permission_classes = (IsAdminUser,)
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+        except:
+            return Response(status = status.HTTP_403_FORBIDDEN)
+        
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+    def put(self,request,pk):
+        self.permission_classes = (IsAdminUser,)
+        try:
+            product = Product.objects.get(id=pk)
+            updated_prod = ProductSerializer(product, data = request.data, partial = True)
+            if updated_prod.is_valid():
+                updated_prod.save()
+        except:
+            return Response(status = status.HTTP_422_UNPROCESSABLE_ENTITY)
+        else:
+            return Response(updated_prod.data, status = status.HTTP_202_ACCEPTED)
