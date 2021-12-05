b689967fc5520e4bbfadbec92061fbbeadd154ce
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-05 17:12:02 +0100

    add and remove items working

--- back-end/cart/models.py
+++ back-end/cart/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE)
-    order_items = models.ManyToManyField("products.Product",blank=True)
+    order_items = models.ManyToManyField("products.Product",related_name="cartItem",blank=True)
 
     def __str__(self):
         return f'{self.user.username}\'s Cart'
