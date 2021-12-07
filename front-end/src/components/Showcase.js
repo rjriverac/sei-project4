f54d2144251315ee555b6e10ce58cae9c242470e
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-07 18:04:54 +0100

    resized images to fit carousel

--- front-end/src/components/Showcase.js
+++ front-end/src/components/Showcase.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Carousel, Container, Image } from 'react-bootstrap'
+import { Carousel, Container, Image, Row, Button, Stack } from 'react-bootstrap'
 
 const Showcase = () => {
 
@@ -22,47 +23,55 @@ const Showcase = () => {
   }, [])
   console.log(products)
   return (
-    <Container style={{ height: '80%' }}>
 
-      {(() => {
-        if (!hasError && products.length) return true
-        else return false
-      })() ?
-        (<Carousel>
-          { products.map((item,idx) => (
-            <Carousel.Item key={idx}>
-              {/* <img 
-                className="d-block img-fluid"
-              /> */}
-              <Image 
-                fluid
-                src={item.big_image}
-                alt={`image of ${item.name}`}
-              />
-              <Carousel.Caption>
-                <h3>{item.name}</h3>
-                <p>{`Price: ${item.price}`}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          ))}
-        </Carousel>)
-        :
-        (
-          <Carousel>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="http://placekeanu.com/800"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>Loading..</h3>
-                <p>Thank you for your patience</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
-        )
-      }
+
+    <Container className='vh-50'>
+      <Row>
+        {(() => {
+          if (!hasError && products.length) return true
+          else return false
+        })() ?
+          (<Carousel>
+            { products.map((item,idx) => (
+              <Carousel.Item key={idx}>
+                <img 
+                  className="d-block img-fluid"
+                  src={item.big_image}
+                  alt={`image of ${item.name}`}
+                />
+                {/* <Image 
+                  
+                /> */}
+                <Carousel.Caption>
+                  <h3>{item.name}</h3>
+                  <p>{`Price: ${item.price}`}</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel>)
+          :
+          (
+            <Carousel>
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  src="http://placekeanu.com/800"
+                  alt="First slide"
+                />
+                <Carousel.Caption>
+                  <h3>Loading..</h3>
+                  <p>Thank you for your patience</p>
+                </Carousel.Caption>
+              </Carousel.Item>
+            </Carousel>
+          )
+        }
+      </Row>
+      <br />
+      <Row>
+        <Button variant='info' href='/home'>Go Back Home</Button>
+      </Row>
+
     </Container>
   )
 }
