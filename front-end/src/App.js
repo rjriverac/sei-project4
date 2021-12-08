b173f96bc377df6968ea714a59fe38a2cd4915ad
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-08 02:19:16 +0100

    added placeholder cart

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Cart from './components/Cart'
 import Home from './components/Home'
 import Login from './components/Login'
 import NavBar from './components/NavBar'
@@ -20,6 +21,7 @@ const App = () => {
           <Route exact path='/login' component={Login}/>
           <Route exact path='/browse' component={Showcase} />
           <Route exact path='/products/:id' component={ProductDetail}/>
+          <Route exact path='/cart' component={Cart}/>
         </Switch>
       </Router>
     </>
