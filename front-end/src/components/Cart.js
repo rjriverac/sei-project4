22b37cd58e7f3e421fb4a87c7e4e0afb69ae1ae3
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-08 16:30:28 +0100

    nested accordion with nonfunctional buttons

--- front-end/src/components/Cart.js
+++ front-end/src/components/Cart.js
@@ -18,7 +18,7 @@ const Cart = () => {
             headers: { Authorization: `Bearer ${token}` }
           }
         )
-        setCart({ items: [...cartItems] , total: total })
+        setCart({ items: [...cartItems], total: total })
       } catch (error) {
         console.log(error)
         setHasError(true)
@@ -35,54 +35,74 @@ const Cart = () => {
         <Col xs={12} md={8}>
           <Accordion>
             <Accordion.Item eventKey='0'>
-              <Accordion.Header>TestHeader</Accordion.Header>
+              <Accordion.Header>
+                <Container>
+                  <Row >
+                    <Col>
+                      Your Cart
+                    </Col>
+                    <Col>
+                      <Button variant='danger'>Empty Cart</Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </Accordion.Header>
               <Accordion.Body>
-                TestBody
+                <Accordion>
+                  {(() => Object.keys(cart).length !== 0)() && cart.items.map((item, index) => {
+                    return (
+                      <Accordion.Item eventKey={index + 1} key={index}>
+                        <Accordion.Header>
+                          <Col>
+                            {item.name}
+                          </Col>
+                          <Col>
+                            <span className='pull-right'><Image src={item.small_image} rounded /></span>
+                          </Col>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          <Row>
+                            <Col md={3}>
+                              <Alert variant='success'>{`$${item.price}`}</Alert>
+                            </Col>
+                            <Col md={{ span: 3, offset: 3 }}>
+                              <Button
+                                variant='danger'
+                                size='sm'
+                              >
+                                Remove from Cart
+                              </Button>
+                            </Col>
+                          </Row>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    )
+                  })}
+                </Accordion>
               </Accordion.Body>
             </Accordion.Item>
-            {(()=> Object.keys(cart).length !== 0)() && cart.items.map((item,index) => {
-              return (
-                <Accordion.Item eventKey={index + 1} key={index}>
-                  <Accordion.Header>
-                    <Col>
-                      {item.name}
-                    </Col>
-                    <Col>
-                      <span className='pull-right'><Image src={item.small_image} rounded /></span>
-                    </Col>
-                  </Accordion.Header>
-                  <Accordion.Body>
-                    <Row>
-                      <Col md={3}>
-                        <Alert variant='success'>{`$${item.price}`}</Alert>
-                      </Col>
-                      <Col md={{ span: 3, offset: 3 }}>
-                        <Button
-                          variant='danger'
-                          size='sm'
-                        >
-                          Remove from Cart
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Accordion.Body>
-                </Accordion.Item>
-              )
-            })}
+
           </Accordion>
         </Col>
         <Col xs={6} md={4}>
           <Alert variant='success'>
             <Alert.Heading>Your Total:</Alert.Heading>
-            {(()=> Object.keys(cart).length !== 0)() && (
-              cart.items.map((item,index)=> {
+            {(() => Object.keys(cart).length !== 0)() && (
+              cart.items.map((item, index) => {
                 return (
                   <p key={index}>{item.name} ${item.price}</p>
                 )
               })
             )}
-            <hr/>
-            <h3>{`$${cart.total}`}</h3>
+            <hr />
+            <Row>
+              <Col>
+                <h3>{`$${cart.total}`}</h3>
+              </Col>
+              <Col>
+                <Button variant='success'>Check Out</Button>
+              </Col>
+            </Row>
           </Alert>
         </Col>
 
