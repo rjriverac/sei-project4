839bd94f33772869ed367942a2a3a5ad148ce26b
Ricardo Rivera-Cabrera <ricardojriveracabrera@gmail.com>
2021-12-09 15:50:53 +0100

    changed logo and navbar

--- front-end/src/components/NavBar.js
+++ front-end/src/components/NavBar.js
@@ -1,8 +1,9 @@
 import React, { useEffect } from 'react'
 import { Navbar, Container, Offcanvas, Nav, NavDropdown } from 'react-bootstrap'
 import { userIsAuthenticated } from './helpers/auth'
-import { useHistory,useLocation } from 'react-router-dom'
-import logo from '../assets/lightlogo2.png'
+import { useHistory, useLocation } from 'react-router-dom'
+// import logo from '../assets/lightlogo2.png'
+import grnlogo from '../assets/greenlogo_gmdgkq.jpg'
 
 const NavBar = () => {
   const history = useHistory()
@@ -17,55 +18,63 @@ const NavBar = () => {
   }
 
   return (
-    <Navbar bg="success" variant="light" expand={false} id='thenavbar'>
-      <Container fluid>
-        <Navbar.Brand>
-          <img
-            src={logo}
-            width="120"
-            height="40"
-            alt="myCart logo"
-            className="d-inline-block align-top"
-          />
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="offcanvasNavbar"/>
-        <Navbar.Offcanvas
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-          placement="end"
-        >
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title
-              id="offcanvasNavbarLabel"
-            >myCart Navigation</Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Nav className="justify-content-end flex-grow-1 pe-2">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/browse">Browse</Nav.Link>
-              {!userIsAuthenticated() ?
-                <>
-                  <Nav.Link href="/register">Register</Nav.Link>
-                  <Nav.Link href="/login">Login</Nav.Link>
-                </>
-                :
-                <>
-                  <NavDropdown title="Your Account" id="offcanvasNavbarDropdown">
-                    <NavDropdown.Item href="/cart">View your cart</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item
-                      as={'a'}
-                      onClick={handleLogout}
-                    >
+
+    <>
+      <style type='text/css'>
+        {`
+          #thenavbar {
+            background-color: #87ba52;
+          }
+        `}
+      </style>
+      <Navbar expand={false} id='thenavbar'>
+        <Container fluid>
+          <Navbar.Brand>
+            <img
+              src={grnlogo}
+              width="120"
+              height="40"
+              alt="myCart logo"
+              className="d-inline-block align-top" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title
+                id="offcanvasNavbarLabel"
+              >myCart Navigation</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-2">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/browse">Browse</Nav.Link>
+                {!userIsAuthenticated() ?
+                  <>
+                    <Nav.Link href="/register">Register</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                  </>
+                  :
+                  <>
+                    <NavDropdown title="Your Account" id="offcanvasNavbarDropdown">
+                      <NavDropdown.Item href="/cart">View your cart</NavDropdown.Item>
+                      <NavDropdown.Divider />
+                      <NavDropdown.Item
+                        as={'a'}
+                        onClick={handleLogout}
+                      >
                         Logout</NavDropdown.Item>
-                  </NavDropdown>
-                </>
-              }
-            </Nav>
-          </Offcanvas.Body>
-        </Navbar.Offcanvas>
-      </Container>
-    </Navbar>
+                    </NavDropdown>
+                  </>}
+              </Nav>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
